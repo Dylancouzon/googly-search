521778419bf69db6a27d7963a19b331e5b82025b
Cheng Tang <cheng21tang@yahoo.com>
2021-05-21 11:36:02 -0700

    added mongoose to package.json and imported mongoose in server.js

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));

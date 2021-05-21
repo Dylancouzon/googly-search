e5cc589c6b8b1d29519437b1d6e2df9820881424
Dylancouzon <dylancouzon@gmail.com>
2021-05-21 10:52:15 -0700

    Add main components && Bootstrap

---
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import CSS from "./Jumbotron.css"
+
+import Container from 'react-bootstrap/Container';
+import BSJumbotron from 'react-bootstrap/Jumbotron';
+
+var styles ={
+    backgroundImage: 'url(./hero.png)',
+    color: 'white',
+    objectFit: 'contain'
+  }
+
+const Jumbotron = () => (
+    <BSJumbotron fluid style={styles}>
+        <Container >
+            <h1>Google Books Search</h1>
+            <p>
+                Search for and save books of interest
+    </p>
+        </Container>
+    </BSJumbotron>
+);
+
+export default Jumbotron;
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+import {Form, Button} from 'react-bootstrap';
+import BSNavbar from 'react-bootstrap/Navbar';
+
+const Navbar = () => (
+    <BSNavbar bg="dark" className="justify-content-between" variant="dark">
+    <BSNavbar.Brand href="#home">
+      <img
+        alt=""
+        src="https://blog.hubspot.com/hubfs/image8-2.jpg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '}
+      Google-Books Search
+    </BSNavbar.Brand>
+    <Form>
+      <Button className="mr-3">Search</Button>
+      <Button >Saved</Button>
+    </Form>
+  </BSNavbar>
+);
+
+export default Navbar;

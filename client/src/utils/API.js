bfbe2d6897d66210f49987dc20793b11a5695411
Dylancouzon <dylancouzon@gmail.com>
2021-05-21 12:55:26 -0700

    Add books api call

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,18 +3,37 @@ import axios from "axios";
 export default {
     // Get book based on search
     getBooks: function(book) {
-        return axios.get('https://www.googleapis.com/books/v1/volumes?q=' + book);
+        axios.get('https://www.googleapis.com/books/v1/volumes?q=' + book)
+        .then(res => {
+            const newData = [];
+            res.data.items.forEach((book)=>{
+              newData.push({
+                    name: book.volumeInfo.title,
+                    author: book.volumeInfo.authors[0],
+                    publishedDate: book.volumeInfo.publishedDate,
+                    description:book.volumeInfo.description,
+                    image: book.volumeInfo.imageLinks,
+                    link: book.selfLink
+              });
+            });
+            console.log(newData)
+            return newData;
+          })
+          .catch(err => console.log(err));
+        
     },
     // Get list of saved books in database
     getSavedBooks: function() {
         return axios.get('/api/books')
     },
     // Save book to database
-    saveBook: function(bookId) {
-        return axios.post('/api/books', bookId)
+    saveBook: function(bookData) {
+        return axios.post('/api/books', bookData)
+        // Mongoose
     },
     // Delete book from database
     deleteBook: function(bookId) {
         return axios.delete('/api/books' + bookId)
+        //Mongoose 
     },
 }

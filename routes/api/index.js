421a240ef179db413ed0a03b482487077cb686f3
Cheng Tang <cheng21tang@yahoo.com>
2021-05-21 15:17:47 -0700

    moved model Book.js to root directory of models get route is working but post is receiving an error of duplicate key

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,28 +1,31 @@
 const router = require('express').Router();
+const Book = require('../../models/Book');
 
-router.get('/books', async (req, res) => {
-  try {
-    const bookData = await Book.findAll()
-    if(!bookData) {
-      res.status(400).json({ message: "hello"});
-    }
-    res.status(200).json(bookData);
-  } catch(err) {
-    res.status(500).json(err);
-  }
+router.get('/books', (req, res) => {
+  // console.log("hello")
+  Book.find({})
+    .then(bookData => {
+      res.status(200).json(bookData);
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 });
 
-router.post('/books', async (req, res) => {
-  try {
-    const bookData = await Book.create(req.body);
-    res.status(200).json(bookData);
-  } catch(err) {
-    res.status(500).json(err);
-  }
+router.post('/books', (req, res) => {
+
+  Book.create(req.body)
+    .then(bookData => {
+      res.status(200).json(bookData);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
 });
 
 router.delete('/books/:id', async (req, res) => {
-  
+
 });
 
 
--- server.js
+++ server.js
@@ -18,12 +18,13 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
-
+app.use('/', require("./routes"));
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
